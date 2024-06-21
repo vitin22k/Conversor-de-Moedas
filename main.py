3b7aa190d4c65a810ad201a33fc1526c61acb698
Vitor Sabor <vitinjolonguinha@gmail.com>
2024-06-21 07:59:36 -0300

    finalizacao de interface e funcionalidades

--- main.py
+++ main.py
@@ -1,47 +1,68 @@
-#janela
-#titulo
-#campos para selecionar as moedas de origem e destino
+#janela => 500 x 500
+#título
+#campos de selecionar moedas de origem e destino
 #botão para converter
 #lista de exibição com os nomes das moedas
-
+ 
 #importar a biblioteca que vai fazer a janela
 import customtkinter
-
-#criar e configurar a janela 
+from pegar_moedas import nomes_moedas, conversoes_disponiveis
+from pegar_cotacao import pegar_cotacao_moeda
+ 
+#criar e configurar a janela
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("green")
-
+ 
 janela = customtkinter.CTk()
 janela.geometry("500x500")
-
-#criar os botões, texto e demais elementos
-titulo = customtkinter.CTkLabel(janela, text="Conversor de Moedas", font=("", 28)) 
-texto_moeda_origem = customtkinter.CTkLabel(janela, text="Selecione a moeda de origem", font=("", 16))
-texto_moeda_destino = customtkinter.CTkLabel(janela, text= "Selecione a moeda de destino", font=("", 16))
-campo_moeda_origem = customtkinter.CTkOptionMenu(janela, values= ["USD", "EUR", "BRL", "BTC"])
-campo_moeda_destino = customtkinter.CTkOptionMenu(janela, values= ["USD", "EUR", "BRL", "BTC"])
-
+janela.title("Conversor De Moedas")
+janela.iconbitmap("money.ico")
+ 
+dic_conversoes_disponiveis = conversoes_disponiveis()
+ 
+#criar os botões, textos e demais elementos
+titulo = customtkinter.CTkLabel(janela, text="Conversor de Moedas", font=("algerian", 25), text_color="#51D793")
+texto_moeda_origem = customtkinter.CTkLabel(janela, text="Selecione a moeda de origem:", font= ("times new roman", 17))
+texto_moeda_destino = customtkinter.CTkLabel(janela, text="Selecione a moeda de destino:", font= ("times new roman", 17))
+ 
+def carregar_moedas_destino(moeda_selecionada):
+    lista_moedas_destino = dic_conversoes_disponiveis[moeda_selecionada]
+    campo_moeda_destino.configure(values=lista_moedas_destino)
+    campo_moeda_destino.set(lista_moedas_destino[0])
+ 
+campo_moeda_origem = customtkinter.CTkOptionMenu(janela, values=list(dic_conversoes_disponiveis.keys()), command=carregar_moedas_destino, font= ("times new roman", 17))
+campo_moeda_destino = customtkinter.CTkOptionMenu(janela, values= ["Selecione uma moeda de origem"], font= ("times new roman", 17))
+ 
 def converter_moeda():
-    print("Converter moeda")
-
-botao_converter = customtkinter.CTkButton(janela, text= "Converter", command=converter_moeda)
-
+    moeda_origem = campo_moeda_origem.get()
+    moeda_destino = campo_moeda_destino.get()
+    if moeda_origem and moeda_destino:
+        cotacao = pegar_cotacao_moeda(moeda_origem, moeda_destino)
+        texto_cotacao_moeda.configure(text=f"1 {moeda_origem}={cotacao} {moeda_destino}")
+ 
+ 
+botao_converter = customtkinter.CTkButton(janela, text="Converter", command=converter_moeda, font=("times new roman", 17))
+ 
 lista_moedas = customtkinter.CTkScrollableFrame(janela)
-
-moedas_disponiveis = ["USD: dólar americano", "EUR: euro", "BRL: real brasileiro", "BTC: Bitcoin"] 
-
-for moeda in moedas_disponiveis:
-    texto_moeda = customtkinter.CTkLabel(lista_moedas, text = moeda)
+ 
+texto_cotacao_moeda = customtkinter.CTkLabel(janela, text="")
+ 
+moedas_disponiveis = nomes_moedas()
+ 
+for codigo_moeda in moedas_disponiveis:
+    nome_moeda = moedas_disponiveis[codigo_moeda]
+    texto_moeda = customtkinter.CTkLabel(lista_moedas, text=f"{codigo_moeda}: {nome_moeda}")
     texto_moeda.pack()
-
+ 
 #colocar os elementos criados na tela
-titulo.pack(padx=10, pady=10)
-texto_moeda_origem.pack(padx=10, pady=0)
+titulo.pack()
+texto_moeda_origem.pack(padx=10, pady=10)
 campo_moeda_origem.pack(padx=10, pady=10)
-texto_moeda_destino.pack(padx=10, pady=0)
+texto_moeda_destino.pack(padx=10, pady=10)
 campo_moeda_destino.pack(padx=10, pady=10)
-botao_converter.pack(padx=10, pady=0)
+botao_converter.pack(padx=10, pady=10)
+texto_cotacao_moeda.pack(padx=10, pady=10)
 lista_moedas.pack(padx=10, pady=10)
-
-#rodar janela
+ 
+#rodar a janela
 janela.mainloop()
